09504812a0291b1d0acff20e18904f3bc5ea9663
saiarun03 <sc22sa2@leeds.ac.uk>
2024-03-21 22:39:46 +0000

    #16 making an initial prototype fo website

--- Software_Project/app/views.py
+++ Software_Project/app/views.py
@@ -2,18 +2,24 @@ from flask import render_template, redirect, url_for, flash, request
 from app import app, db
 from app.models import User
 from app.forms import RegistrationForm
-from werkzeug.security import generate_password_hash
+# from werkzeug import security
+# from werkzeug.security import generate_password_hash
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
+
+@app.route('/friends', methods=['GET'])
+def friends():
+    return render_template('friends.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
-        hashed_password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
+        # hashed_password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
         user = User(
             username=form.username.data,
             firstname=form.firstName.data,
