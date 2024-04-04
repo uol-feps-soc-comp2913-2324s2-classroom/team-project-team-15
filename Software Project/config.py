db9a03a0d09e0d4b7765eb8e3aa7cccc6d43bd10
mohidChaudhry <124250910+mohidChaudhry@users.noreply.github.com>
2024-04-04 01:30:55 +0100

    config file
    Co-Authored-By: AbdulRafay0001 <124687669+AbdulRafay0001@users.noreply.github.com>
    Co-Authored-By: sc22eg <123957302+Eesha-Gupta@users.noreply.github.com>
    Co-Authored-By: saiarun03 <123959862+saiarun03@users.noreply.github.com>
    Co-Authored-By: ll17h22s <124059110+ll17h22s@users.noreply.github.com>

---
+++ Software Project/config.py
@@ -0,0 +1,6 @@
+WTF_CRSF_ENABLED=True
+SECRET_KEY='team15project'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
